74320ea7eebcba33e71e812642ad9f1028c3501e
phantom1407 <marialecisnerosh@gmail.com>
2026-08-20 10:10:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ st.title("HOLA! mi nombre es Maleja")
 
 image = Image.open ('cat.jpg')
 st.image(image, caption='Interfaces multimodales y un gatito')
+
+texto=st.text_input('buenas buenas','este es mi texto')
+st.write('el texto escrito es ', texto)
