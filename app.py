c69913d0d180189a3c1baaecec2faffd439c02a8
phantom1407 <marialecisnerosh@gmail.com>
2026-08-20 10:20:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,21 @@ st.image(image, caption='Interfaces multimodales y un gatito')
 
 texto=st.text_input('buenas buenas','awawawawawa')
 st.write('el texto escrito es ', texto)
+
+st.subheader('ahora dos columnas:')
+col1, col2=st.columns(2)
+with col1:
+  st.write('las interfaces multimodales mejoran la exp de usuario')
+  resp=st.checkbox('sisas')
+  if resp:
+    st.write('niceee')
+
+with col2:
+  modo=st.subheader('que modalidad es la principal en esta interfaz?', ('visual', 'auditiva', 'tactil'))
+  if modo== 'visual':
+    st.write('la vista es fundamental en esta interfaz')
+  if modo== 'auditiva':
+    st.write('la audición no es fundamental en esta interfaz')
+  if modo== 'tactil':
+    st.write('el sentir no es fundamental en esta interfaz')
+
